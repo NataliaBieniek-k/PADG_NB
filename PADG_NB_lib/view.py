2f43ac7291e53e647418688b902284736efb2d45
NataliaBieniek-k <u56113@student.wat.edu.pl>
2025-12-01 12:45:59 +0100

    feat: add GUI for Clinic

--- PADG_NB_lib/view.py
+++ PADG_NB_lib/view.py
@@ -9,13 +9,11 @@ def start_gui():
 
     ramka_lista_obiektow = Frame(root)
     ramka_formularz = Frame(root)
-    ramka_szczegoly_obiektu = Frame(root)
     ramka_mapa = Frame(root)
 
     ramka_lista_obiektow.grid(row=0, column=0)
     ramka_formularz.grid(row=0, column=1)
-    ramka_szczegoly_obiektu.grid(row=1, column=0, columnspan=2)
-    ramka_mapa.grid(row=2, column=0, columnspan=2)
+    ramka_mapa.grid(row=1, column=0, columnspan=2)
 
     map_widget = tkintermapview.TkinterMapView(
         ramka_mapa, width=1025, height=600, corner_radius=0
@@ -29,68 +27,27 @@ def start_gui():
 
     view = View()
 
-    Label(ramka_lista_obiektow, text="Lista obiektów").grid(row=0, column=0, columnspan=3)
-    view.list_box_lista_obiektow = Listbox(ramka_lista_obiektow)
-    view.list_box_lista_obiektow.grid(row=1, column=0, columnspan=3)
+    Label(ramka_lista_obiektow, text="Lista przychodni").grid(row=0, column=0, columnspan=2)
+    view.list_box_clinics = Listbox(ramka_lista_obiektow, width=40)
+    view.list_box_clinics.grid(row=1, column=0, columnspan=2)
 
-    label_formularz = Label(ramka_formularz, text="Formularz: ")
-    label_formularz.grid(row=0, column=0, columnspan=2)
+    Label(ramka_formularz, text="Nazwa przychodni:").grid(row=0, column=0, sticky=W)
+    view.entry_clinic_name = Entry(ramka_formularz)
+    view.entry_clinic_name.grid(row=0, column=1)
 
-    Label(ramka_formularz, text="Imie: ").grid(row=1, column=0, sticky=W)
-    Label(ramka_formularz, text="Lokalizacja: ").grid(row=2, column=0, sticky=W)
-    Label(ramka_formularz, text="Posty: ").grid(row=3, column=0, sticky=W)
-    Label(ramka_formularz, text="Obrazek: ").grid(row=4, column=0, sticky=W)
-
-    view.entry_name = Entry(ramka_formularz)
-    view.entry_name.grid(row=1, column=1)
-
-    view.entry_lokalizacja = Entry(ramka_formularz)
-    view.entry_lokalizacja.grid(row=2, column=1)
-
-    view.entry_posty = Entry(ramka_formularz)
-    view.entry_posty.grid(row=3, column=1)
-
-    view.entry_img_url = Entry(ramka_formularz)
-    view.entry_img_url.grid(row=4, column=1)
-
-    Label(ramka_szczegoly_obiektu, text="Szczegóły obiektu: ").grid(row=0, column=0, sticky=W)
-    Label(ramka_szczegoly_obiektu, text="Imie: ").grid(row=1, column=0)
-    view.label_imie_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-    view.label_imie_szczegoly_obiektu_wartosc.grid(row=1, column=1)
-
-    Label(ramka_szczegoly_obiektu, text="Lokalizacja: ").grid(row=1, column=3)
-    view.label_lokalizacja_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-    view.label_lokalizacja_szczegoly_obiektu_wartosc.grid(row=1, column=4)
-
-    Label(ramka_szczegoly_obiektu, text="Posty: ").grid(row=1, column=5)
-    view.label_posty_szczegoly_obiektu_wartosc = Label(ramka_szczegoly_obiektu, text="...")
-    view.label_posty_szczegoly_obiektu_wartosc.grid(row=1, column=6)
+    Label(ramka_formularz, text="Miasto:").grid(row=1, column=0, sticky=W)
+    view.entry_clinic_city = Entry(ramka_formularz)
+    view.entry_clinic_city.grid(row=1, column=1)
 
     controller = Controller(view, map_widget)
 
-    Button(
-        ramka_lista_obiektow,
-        text="Pokaż szczegóły",
-        command=controller.user_details
-    ).grid(row=2, column=0)
+    view.button_add_clinic = Button(ramka_formularz, text="Dodaj przychodnię", command=controller.add_clinic)
+    view.button_add_clinic.grid(row=2, column=0, columnspan=2, pady=5)
 
-    Button(
-        ramka_lista_obiektow,
-        text="Usuń obiekt",
-        command=controller.delete_user
-    ).grid(row=2, column=1)
+    view.button_edit_clinic = Button(ramka_formularz, text="Edytuj przychodnię", command=controller.edit_clinic)
+    view.button_edit_clinic.grid(row=3, column=0, columnspan=2, pady=5)
 
-    Button(
-        ramka_lista_obiektow,
-        text="Edytuj obiekt",
-        command=controller.edit_user
-    ).grid(row=2, column=2)
-
-    view.button_dodaj_obiekt = Button(
-        ramka_formularz,
-        text="Dodaj obiekt",
-        command=controller.add_user
-    )
-    view.button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
+    view.button_delete_clinic = Button(ramka_formularz, text="Usuń przychodnię", command=controller.delete_clinic)
+    view.button_delete_clinic.grid(row=4, column=0, columnspan=2, pady=5)
 
     root.mainloop()
